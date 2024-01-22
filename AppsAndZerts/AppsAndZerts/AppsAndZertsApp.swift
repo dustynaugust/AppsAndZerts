858272a0c42d4cc9a90f99fe24dcba35743ccb16
Dustyn August <dustyn.august@gmail.com>
2024-01-22 14:25:58 -0800

    Adds initial DessertFeed.swift

--- AppsAndZerts/AppsAndZerts/AppsAndZertsApp.swift
+++ AppsAndZerts/AppsAndZerts/AppsAndZertsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppsAndZertsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DessertFeed()
         }
     }
 }
---
+++ AppsAndZerts/AppsAndZerts/DessertFeed/DessertFeed.swift
@@ -0,0 +1,19 @@
+//
+//  DessertFeed.swift
+//  AppsAndZerts
+//
+//  Created by Dustyn August on 1/22/24.
+//
+
+import SwiftUI
+
+struct DessertFeed: View {
+    var body: some View {
+        Text("Desert Feed Placeholder")
+    }
+}
+
+#Preview {
+    DessertFeed()
+}
+
